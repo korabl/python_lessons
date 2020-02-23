da68083d9f1b5abf87e9c650369384dea7059af2
Korablina <kooorablina@yandex.ru>
2020-02-23 18:17:35 +0300

    12

--- lesson_1/task6.py
+++
@@ -1,9 +0,0 @@
-current_result = int(input('Введите текущее кол-во км: '))
-wish_result = int(input('Введите желаемое кол-во км: '))
-
-days = 1
-while current_result <= wish_result:
-    current_result *= 1.1
-    days += 1
-
-print(days)
